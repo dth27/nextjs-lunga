42f0d0d44cfd7a22f4de8975392ef3d04587fd41
diljath <diljath@advania.is>
2022-07-02 11:47:02 +0000

    is default

--- utils/localization/i18n-config.ts
+++ utils/localization/i18n-config.ts
@@ -3,8 +3,8 @@ import { initReactI18next } from 'react-i18next';
 import { translationsOptions } from './translate';
 
 i18next.use(initReactI18next).init({
-  lng: 'en',
-  fallbackLng: 'is',
+  lng: 'is',
+  fallbackLng: 'en',
   nsSeparator: ':',
   ...translationsOptions,
 });
